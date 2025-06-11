0c462dacce74ce94792cfb6377d95a45b97ae7e4
kotjuz <rkotarski10@gmail.com>
2025-06-11 23:41:24 +0200

    fixed gorq

--- models/marka.go
+++ models/marka.go
@@ -2,5 +2,5 @@ package models
 
 type Marka struct {
 	nazwa        string `gorm:"primaryKey"`
-	rokZalozenia int    `gorm:"not null;check:year_founded > 1900"`
+	rokZalozenia int    `gorm:"not null;check:rokZalozenia > 1900"`
 }
