98cfe0c386d9a48d37ae152700b26c04400a7d25
kotjuz <rkotarski10@gmail.com>
2025-06-12 08:35:17 +0200

    feat: connected to db

--- database/database.go
+++ database/database.go
@@ -1,1 +1,40 @@
 package database
+
+import (
+	"fmt"
+	"os"
+
+	"example.com/ormproject/models"
+	"gorm.io/driver/sqlserver"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	instance := os.Getenv("DB_INSTANCE")
+	database := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("sqlserver://%s:%s@%s\\%s?database=%s&TrustServerCertificate=true",
+		user, password, host, instance, database)
+
+	db, err := gorm.Open(sqlserver.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("Could not connect to database")
+	}
+
+	DB = db
+
+	db.AutoMigrate(
+		&models.Samochod{},
+		&models.PosSilnik{},
+		&models.TypSilnika{},
+		&models.Dealer{},
+		&models.Model{},
+	)
+}
